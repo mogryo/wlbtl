19c8f66a5e1a3390a7880e3c0fac1e4f9978b5ac
mogryo <tad.baikstis@gmail.com>
2024-12-27 12:14:12 +0200

    Add map

--- src/scenes/DevelopmentScene.ts
+++ src/scenes/DevelopmentScene.ts
@@ -6,6 +6,7 @@ import type { PlayerCursors } from "src/types/characters";
 export default class DevelopmentScene extends Phaser.Scene {
     private cursors?: PlayerCursors;
     private player?: PlayerCharacter;
+    private wallsLayer?: Phaser.Tilemaps.TilemapLayer | null = null;
     constructor() {
         super("development-scene");
     }
@@ -21,11 +22,34 @@ export default class DevelopmentScene extends Phaser.Scene {
         }) as PlayerCursors;
     }
 
-    create(): void {
+    private createMap() {
+        const map = this.make.tilemap({
+            key: "dungeon-tile-map", // key defined in loader scene
+        });
+
+        // key - defined in loader scene, tilesetName - the name of tileset in Tiled
+        const tileSet = map.addTilesetImage("demo-tileset", "tiles", 32, 32);
+        if (!tileSet) return;
+
+        // Layer id - name of the layer in Tiled
+        map.createLayer("Floor", tileSet);
+        this.wallsLayer = map.createLayer("Walls", tileSet);
+        this.wallsLayer?.setCollisionByProperty({ collides: true });
+    }
+
+    private createPlayer() {
         createDecertAnimations(this.anims);
-        this.player = new PlayerCharacter(this, 200, 300, "decert");
+        this.player = new PlayerCharacter(this, 100, 200, "decert");
         this.physics.world.enableBody(this.player);
         this.player.body?.setSize(this.player.width * 0.5, this.player.height * 0.8);
+        this.cameras.main.startFollow(this.player, true);
+    }
+
+    create(): void {
+        this.createMap();
+        this.createPlayer();
+
+        if (this.wallsLayer && this.player) this.physics.add.collider(this.player, this.wallsLayer);
     }
 
     override update(time: number, delta: number) {
