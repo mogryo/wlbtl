04b85e8c7bb7a9b9c0630828a5b965d89988151a
mogryo <tad.baikstis@gmail.com>
2025-01-03 11:25:43 +0200

    Rename swing typo

--- src/weapons/melee/HyperKnife.ts
+++ src/weapons/melee/HyperKnife.ts
@@ -2,17 +2,17 @@ import type * as Phaser from "phaser";
 import { StabTypeProjectile } from "src/weapons/melee-projectiles/StabTypeProjectile";
 
 export class HyperKnife {
-    private swingGroup?: Phaser.Physics.Arcade.Group;
+    private stabGroup?: Phaser.Physics.Arcade.Group;
 
     constructor(scene: Phaser.Scene) {
-        this.swingGroup = scene.physics.add.group({ classType: StabTypeProjectile, runChildUpdate: true, maxSize: 1 });
+        this.stabGroup = scene.physics.add.group({ classType: StabTypeProjectile, runChildUpdate: true, maxSize: 1 });
     }
 
     public attack(
         attacker: Phaser.GameObjects.Sprite,
         crosshair: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody,
     ): void {
-        const knife: StabTypeProjectile | undefined = this.swingGroup?.get(attacker.x, attacker.y, "swing");
+        const knife: StabTypeProjectile | undefined = this.stabGroup?.get(attacker.x, attacker.y, "stab");
         if (knife) {
             knife.setTexture("hyper-knife");
             (knife.body as Phaser.Physics.Arcade.Body)?.setSize(knife.width * 0.4, knife.height * 0.9);
